708bbc11eb98d894a63d65ccbeeba49d82f246c0
Tipsy910 <159101951+Tipsy910@users.noreply.github.com>
2024-02-13 17:03:19 +0700

    Add files via upload

---
+++ app01.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.header("OOP IN ML")
+st.sidebar.text("Sidebar")
+st.sidebar.button("go>")
+st.sidebar.button("went>")
+st.sidebar.button("gone>")
+st.image('http://picsum.photos/500/200')
+st.text_input("What is you name")
+# git config --gobal user.name Tipsy910
+# git config --gobal user.email senbonzakura.tipsy.910@gmail.com
