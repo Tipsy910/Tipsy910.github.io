2e7f5e10aa3d8d98000fea6e8302cacb8c91cc7e
Tipsy910 <159101951+Tipsy910@users.noreply.github.com>
2024-02-13 17:02:17 +0700

    Delete Tipsy910.github.io directory

--- Tipsy910.github.io/streamlit/app01.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.header("OOP IN ML")
-st.sidebar.text("Sidebar")
-st.sidebar.button("go>")
-st.sidebar.button("went>")
-st.sidebar.button("gone>")
-st.image('http://picsum.photos/500/200')
-st.text_input("What is you name")
